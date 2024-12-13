6add18507227f2119374ae55380b420b849de35f
Ekaterina Lys <Ekaterin.Lys@yandex.ru>
2024-12-13 05:31:39 +0300

    app.module, main, pachages, tsconfig, gitignore were changed

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,13 +6,13 @@ import { ConfigModule } from '@nestjs/config';
 import { HotelsModule } from './hotels/hotels.module';
 import { ReservationModule } from './reservation/reservation.module';
 import { SupportChatModule } from './support-chat/support-chat.module';
-
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_CONNECTION),
-    UserModule, HotelsModule, ReservationModule, SupportChatModule
+    UserModule, HotelsModule, ReservationModule, SupportChatModule, AuthModule
 ],
 })
 export class AppModule {}
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
+/* eslint-disable prettier/prettier */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import session from 'express-session';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+
+    app.use(
+        session({
+            secret: process.env.SESSION_SECRET,
+            resave: false,
+            saveUninitialized: false,
+            cookie: { secure: false },
+        })
+    )
+    await app.listen(3000);
 }
+    
 bootstrap();
